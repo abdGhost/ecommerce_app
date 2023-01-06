bf9859a4854f3b196229f8fc7cd287f9aebbaba9
abdul <ktaher642@gmail.com>
2023-01-06 13:03:08 +0530

    Added bottom navigation in recommended food details screen

--- lib/widget/appbar_icon.dart
+++ lib/widget/appbar_icon.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
-import '../util/dimensions.dart';
-
 class AppbarIcon extends StatelessWidget {
   final IconData icon;
   final Color backgroundColor;
   final Color iconColor;
   final double size;
+  final double iconSize;
 
   const AppbarIcon({
     super.key,
@@ -14,6 +13,7 @@ class AppbarIcon extends StatelessWidget {
     this.backgroundColor = const Color(0xFFfcf4e4),
     this.iconColor = const Color(0xFF756d54),
     this.size = 40,
+    this.iconSize = 16,
   });
 
   @override
@@ -27,7 +27,7 @@ class AppbarIcon extends StatelessWidget {
       ),
       child: Icon(
         icon,
-        size: Dimensions.iconSize16,
+        size: iconSize,
         color: iconColor,
       ),
     );

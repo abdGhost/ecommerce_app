124d2d71bcda9d493c6ab725b2f803cf22b5b120
abdul <ktaher642@gmail.com>
2023-01-07 18:37:51 +0530

    Solve error regarding the response

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -2,10 +2,11 @@ import 'package:get/get.dart';
 import 'package:shop_app/controllers/popular_product_controller.dart';
 import 'package:shop_app/data/api/api_client.dart';
 import 'package:shop_app/data/repository/popular_product_repo.dart';
+import 'package:shop_app/util/app_constants.dart';
 
 Future<void> init() async {
   //API Client
-  Get.lazyPut(() => ApiClient(appBaseUrl: "http://mvs.bslmeiyu.com/"));
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
   //Repos
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -1,8 +1,8 @@
 class AppConstants {
   static const String APP_NAME = "GhostFood";
-  static const int APP_VERSION =1;
+  static const int APP_VERSION = 1;
 
-  static const String BASE_URL = "https://mvs.bslmeiyu.com";
-  static const String POPULAR_PRODUCT_URI= "/api/v1/products/popular";
-  static const String RECOMMENDED_PRODUCT_URI ="/api/v1/products/recommened";
+  static const String BASE_URL = "http://mvs.bslmeiyu.com";
+  static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
+  static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommened";
 }

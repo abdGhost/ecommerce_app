5faf9f256688cabf6726482c1127dddbe021c1f4
abdul <ktaher642@gmail.com>
2023-02-17 12:11:48 +0530

    Solving error

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MainFoodPage extends StatelessWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:shop_app/controllers/popular_product_controller.dart';
-import 'package:shop_app/pages/food/recommended_food_details.dart';
 
-import 'helper/dependencies.dart' as dep;
+import 'home/main_food_page.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await dep.init();
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Get.find<PopularProductController>().getPopularProductList();
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shop-App',
+    return MaterialApp(
+      title: 'Shop App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const RecommenedFoodDetail(),
+      home: const MainFoodPage(),
     );
   }
 }
